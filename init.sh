f7dd713e88af8fbd2164fc79d0eacd12520410c9
2bit@M1 <github@2bit.jp>
2021-04-03 17:38:42 +0900

    update init script

--- init.sh
+++ init.sh
@@ -2,8 +2,34 @@
 
 set -e
 
+NEED_COPY_LIST=(".npm-init.js")
+
+IGNORE_LIST=("." ".." ".git" ".gitignore" "init.sh")
+
+function is_ignored() {
+    local i
+    for i in ${IGNORE_LIST[@]}; do
+        if [[ ${i} = ${1} ]]; then
+            echo 1
+            return
+        fi
+    done
+    echo 0
+}
+
+function is_copy_needed() {
+    local i
+    for i in ${NEED_COPY_LIST[@]}; do
+        if [[ ${i} = ${1} ]]; then
+            echo 1
+            return
+        fi
+    done
+    echo 0
+}
+
 function symlink() {
-    echo symlink \"$1\"
+    echo "* symlink \"$1\""
     if [ -f ${HOME}/$1 ]; then
         echo "  ${HOME}/$1 is exist [file]"
     elif [ -L ${HOME}/$1 ]; then
@@ -15,11 +41,11 @@ function symlink() {
 }
 
 function copyfile() {
-    echo copy \"$1\"
+    echo "* copy \"$1\""
     if [ -e ${HOME}/$1 ]; then
         echo "  ${HOME}/$1 is exist"
     else
-        echo "  symbloic link to ${HOME}/$1"
+        echo "  copyfile to ${HOME}/$1"
         # cp ${DOTFILES_DIR}/$1 ${HOME}/$1
     fi
 }
@@ -29,7 +55,22 @@ cd $(dirname $0)
 
 DOTFILES_DIR=`pwd`
 
-symlink .vimrc
-symlink Brewfile
-copyfile .npm-init.js
+for FILE in `ls -a ${DOTFILES_DIR}`
+do
+    if [ `is_ignored ${FILE}` = "0" ]
+    then
+        COPY_NEEDED=`is_copy_needed ${FILE}`
+        if [ `is_copy_needed ${FILE}` = "1" ]
+        then
+            copyfile ${FILE}
+        else
+            symlink ${FILE}
+        fi
+    # else
+    #     echo "* * ${FILE} is ignored"
+    fi
+done
 
+# symlink .vimrc
+# symlink Brewfile
+# copyfile .npm-init.js
